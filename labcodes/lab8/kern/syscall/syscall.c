7d5f18d283950859e442de9a22d0280d50cc84c1
Wende Tan <twd2@163.com>
2018-03-06 17:10:19 +0800

    "strace"

--- labcodes/lab8/kern/syscall/syscall.c
+++ labcodes/lab8/kern/syscall/syscall.c
@@ -14,11 +14,13 @@
 static int
 sys_exit(uint32_t arg[]) {
     int error_code = (int)arg[0];
+    cprintf("pid=%d, exit(status=%d)\n", current->pid, error_code);
     return do_exit(error_code);
 }
 
 static int
 sys_fork(uint32_t arg[]) {
+    cprintf("pid=%d, fork()\n", current->pid);
     struct trapframe *tf = current->tf;
     uintptr_t stack = tf->tf_esp;
     return do_fork(0, stack, tf);
@@ -28,6 +30,7 @@ static int
 sys_wait(uint32_t arg[]) {
     int pid = (int)arg[0];
     int *store = (int *)arg[1];
+    cprintf("pid=%d, wait(pid=%d, store=0x%08x)\n", current->pid, pid, store);
     return do_wait(pid, store);
 }
 
@@ -36,28 +39,33 @@ sys_exec(uint32_t arg[]) {
     const char *name = (const char *)arg[0];
     int argc = (int)arg[1];
     const char **argv = (const char **)arg[2];
+    cprintf("pid=%d, exec(name=\"%s\", argc=%d, argv=0x%08x)\n", current->pid, name, argc, argv);
     return do_execve(name, argc, argv);
 }
 
 static int
 sys_yield(uint32_t arg[]) {
+    cprintf("pid=%d, yield()\n", current->pid);
     return do_yield();
 }
 
 static int
 sys_kill(uint32_t arg[]) {
     int pid = (int)arg[0];
+    cprintf("pid=%d, kill(pid=%d)\n", current->pid, pid);
     return do_kill(pid);
 }
 
 static int
 sys_getpid(uint32_t arg[]) {
+    cprintf("pid=%d, getpid()\n", current->pid);
     return current->pid;
 }
 
 static int
 sys_putc(uint32_t arg[]) {
     int c = (int)arg[0];
+    // cprintf("pid=%d, putc(c='%c')\n", current->pid, c);
     cputchar(c);
     return 0;
 }
@@ -65,17 +73,20 @@ sys_putc(uint32_t arg[]) {
 static int
 sys_pgdir(uint32_t arg[]) {
     print_pgdir();
+    cprintf("pid=%d, pgdir()\n", current->pid);
     return 0;
 }
 
 static int
 sys_gettime(uint32_t arg[]) {
+    cprintf("pid=%d, gettime()\n", current->pid);
     return (int)ticks;
 }
 static int
 sys_lab6_set_priority(uint32_t arg[])
 {
     uint32_t priority = (uint32_t)arg[0];
+    cprintf("pid=%d, lab6_set_priority(priority=%d)\n", current->pid, priority);
     lab6_set_priority(priority);
     return 0;
 }
@@ -83,6 +94,7 @@ sys_lab6_set_priority(uint32_t arg[])
 static int
 sys_sleep(uint32_t arg[]) {
     unsigned int time = (unsigned int)arg[0];
+    cprintf("pid=%d, sleep(time=%u)\n", current->pid, time);
     return do_sleep(time);
 }
 
@@ -90,12 +102,14 @@ static int
 sys_open(uint32_t arg[]) {
     const char *path = (const char *)arg[0];
     uint32_t open_flags = (uint32_t)arg[1];
+    cprintf("pid=%d, open(path=\"%s\", flags=%u)\n", current->pid, path, open_flags);
     return sysfile_open(path, open_flags);
 }
 
 static int
 sys_close(uint32_t arg[]) {
     int fd = (int)arg[0];
+    cprintf("pid=%d, close(fd=%d)\n", current->pid, fd);
     return sysfile_close(fd);
 }
 
@@ -104,6 +118,9 @@ sys_read(uint32_t arg[]) {
     int fd = (int)arg[0];
     void *base = (void *)arg[1];
     size_t len = (size_t)arg[2];
+    if (fd != 0 && fd != 1 && fd != 2) {
+        cprintf("pid=%d, read(fd=%d, base=0x%08x, len=%u)\n", current->pid, fd, base, len);
+    }
     return sysfile_read(fd, base, len);
 }
 
@@ -112,6 +129,9 @@ sys_write(uint32_t arg[]) {
     int fd = (int)arg[0];
     void *base = (void *)arg[1];
     size_t len = (size_t)arg[2];
+    if (fd != 0 && fd != 1 && fd != 2) {
+        cprintf("pid=%d, write(fd=%d, base=0x%08x, len=%u)\n", current->pid, fd, base, len);
+    }
     return sysfile_write(fd, base, len);
 }
 
@@ -120,6 +140,7 @@ sys_seek(uint32_t arg[]) {
     int fd = (int)arg[0];
     off_t pos = (off_t)arg[1];
     int whence = (int)arg[2];
+    cprintf("pid=%d, seek(fd=%d, pos=%d, whence=%d)\n", current->pid, fd, pos, whence);
     return sysfile_seek(fd, pos, whence);
 }
 
@@ -127,12 +148,14 @@ static int
 sys_fstat(uint32_t arg[]) {
     int fd = (int)arg[0];
     struct stat *stat = (struct stat *)arg[1];
+    cprintf("pid=%d, fstat(fd=%d, stat=0x%08x)\n", current->pid, fd, stat);
     return sysfile_fstat(fd, stat);
 }
 
 static int
 sys_fsync(uint32_t arg[]) {
     int fd = (int)arg[0];
+    cprintf("pid=%d, fsync(fd=%d)\n", current->pid, fd);
     return sysfile_fsync(fd);
 }
 
@@ -140,6 +163,7 @@ static int
 sys_getcwd(uint32_t arg[]) {
     char *buf = (char *)arg[0];
     size_t len = (size_t)arg[1];
+    cprintf("pid=%d, getcwd(buf=0x%08x, len=%d)\n", current->pid, buf, len);
     return sysfile_getcwd(buf, len);
 }
 
@@ -147,6 +171,7 @@ static int
 sys_getdirentry(uint32_t arg[]) {
     int fd = (int)arg[0];
     struct dirent *direntp = (struct dirent *)arg[1];
+    cprintf("pid=%d, getcwd(fd=%d, direntp=0x%08x)\n", current->pid, fd, direntp);
     return sysfile_getdirentry(fd, direntp);
 }
 
@@ -154,6 +179,7 @@ static int
 sys_dup(uint32_t arg[]) {
     int fd1 = (int)arg[0];
     int fd2 = (int)arg[1];
+    cprintf("pid=%d, dup2(fd1=%d, fd2=%d)\n", current->pid, fd1, fd2);
     return sysfile_dup(fd1, fd2);
 }
 
