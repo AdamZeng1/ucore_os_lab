62d2e6a0b03927817abd50cff8354da9c49eb0ff
Wende Tan <twd2@163.com>
2018-03-20 22:27:18 +0800

    kill -KERNBASE GDT

--- labcodes/lab8/kern/mm/pmm.c
+++ labcodes/lab8/kern/mm/pmm.c
@@ -40,7 +40,8 @@ struct Page *pages;
 size_t npage = 0;
 
 // virtual address of boot-time page directory
-pde_t *boot_pgdir = NULL;
+extern pde_t __boot_pgdir;
+pde_t *boot_pgdir = &__boot_pgdir;
 // physical address of boot-time page directory
 uintptr_t boot_cr3;
 
@@ -298,6 +299,8 @@ boot_alloc_page(void) {
 //         - check the correctness of pmm & paging mechanism, print PDT&PT
 void
 pmm_init(void) {
+    boot_cr3 = PADDR(boot_pgdir);
+
     //We need to alloc/free the physical memory (granularity is 4KB or other size). 
     //So a framework of physical memory manager (struct pmm_manager)is defined in pmm.h
     //First we should init a physical memory manager(pmm) based on the framework.
@@ -312,11 +315,6 @@ pmm_init(void) {
     //use pmm->check to verify the correctness of the alloc/free function in a pmm
     check_alloc_page();
 
-    // create boot_pgdir, an initial page directory(Page Directory Table, PDT)
-    boot_pgdir = boot_alloc_page();
-    memset(boot_pgdir, 0, PGSIZE);
-    boot_cr3 = PADDR(boot_pgdir);
-
     check_pgdir();
 
     static_assert(KERNBASE % PTSIZE == 0 && KERNTOP % PTSIZE == 0);
@@ -326,23 +324,14 @@ pmm_init(void) {
     boot_pgdir[PDX(VPT)] = PADDR(boot_pgdir) | PTE_P | PTE_W;
 
     // map all physical memory to linear memory with base linear addr KERNBASE
-    //linear_addr KERNBASE~KERNBASE+KMEMSIZE = phy_addr 0~KMEMSIZE
-    //But shouldn't use this map until enable_paging() & gdt_init() finished.
+    // linear_addr KERNBASE ~ KERNBASE + KMEMSIZE = phy_addr 0 ~ KMEMSIZE
     boot_map_segment(boot_pgdir, KERNBASE, KMEMSIZE, 0, PTE_W);
 
-    //temporary map: 
-    //virtual_addr 3G~3G+4M = linear_addr 0~4M = linear_addr 3G~3G+4M = phy_addr 0~4M     
-    boot_pgdir[0] = boot_pgdir[PDX(KERNBASE)];
-
-    enable_paging();
-
-    //reload gdt(third time,the last time) to map all physical memory
-    //virtual_addr 0~4G=liear_addr 0~4G
-    //then set kernel stack(ss:esp) in TSS, setup TSS in gdt, load TSS
+    // Since we are using bootloader's GDT,
+    // we should reload gdt (second time, the last time) to map all physical memory
+    // virtual_addr 0 ~ 4G = linear_addr 0 ~ 4G
+    // then set kernel stack(ss:esp) in TSS, setup TSS in gdt, load TSS
     gdt_init();
-
-    //disable the map of virtual_addr 0~4M
-    boot_pgdir[0] = 0;
 
     //now the basic virtual memory map(see memalyout.h) is established.
     //check the correctness of the basic virtual memory map.
